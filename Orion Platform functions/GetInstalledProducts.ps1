8e9d075a8fbca4834cf9659b71406d31bf005671
BohumilKlimek <klimek.bohumil@gmail.com>
2015-07-10 00:52:23 +0200

    Next major step is done.
    Now only separating components plugins + its template is missing.

--- Orion Platform functions/GetInstalledProducts.ps1
+++ Orion Platform functions/GetInstalledProducts.ps1
@@ -1,9 +1,10 @@
 #region Function info
     ### PURPOSE:
         ### This function returns all installed products.
-        ### It's done via two simple steps:
+        ### It's done via three simple steps:
             ### Information about configured modules is collected ("HKLM\SOFTWARE\Wow6432Node\SolarWinds.Net\ConfigurationWizard\ConfiguredModules").
-            ### Based on transformation ("Components.xml" in repository; https://cp.solarwinds.com/x/jqzgAg on Confluence) product(s) version is determined.
+            ### Based on information in "Components.xml" is determined if configured module is product or not.
+            ### Products version is obtained from module.xml.
 
     ### INPUT:
         ### No input parameters.
@@ -25,7 +26,7 @@
 
 function GetInstalledProducts ()
 { 
-    ### Determining SolarWinds registry root folder:
+    ### Determining SolarWinds registry root folders and installation path:
         if (GetBitPlatfrom -eq "64bit")
 	    {
 		    [System.String] $SolarWindsRegistryRoot = "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\SolarWinds\"
@@ -36,6 +37,9 @@ function GetInstalledProducts ()
 		    [System.String] $SolarWindsRegistryRoot = "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\SolarWinds\"
             [System.String] $SolarWindsNetRegistryRoot = "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\SolarWinds.Net\"
 	    }
+
+    ### Determining installation path based on bit platform:
+        [System.String] $InstallationPath = (Get-ItemProperty -Path ($SolarWindsRegistryRoot + "Orion\Core")).InstallPath
 
     ### Reading configured modules values:
         [System.String[]] $ConfiguredModules = (Get-item -Path ($SolarWindsNetRegistryRoot + "ConfigurationWizard\ConfiguredModules")).Property
@@ -53,8 +57,14 @@ function GetInstalledProducts ()
             {
                 if ($ConfiguredModules.Contains($Component.Configuration.ID))
                 {
-                    ### Updating output variable with needed data:
-                        Add-Member -InputObject $InstalledProducts -MemberType NoteProperty -Name $Component.Name -Value "TBD"
+                    ### Loading component's module.xml to get external product version:
+                        [System.Xml.XmlDocument] $ModuleConfiguration = Get-Content -Path ($InstallationPath + "Modules\" + $Component.ModuleName + ".xml")
+                    
+                        ### Getting external product version from module.xml:
+                            $ExternalProductVersion = $ModuleConfiguration.OrionModule.Info.ModuleVersion 
+                    
+                    ### Updating output variable with all data:
+                        Add-Member -InputObject $InstalledProducts -MemberType NoteProperty -Name $Component.Name -Value $ExternalProductVersion
                 }
             }
         }
--- README.md
+++ README.md
@@ -8,10 +8,6 @@
 * Add function for detecting correct version of PowerShell (script execution will be blocked, if lover than 4.0).
 * Start working on “GetComponentsVersion.ps1”, which will be in Tools container.
 * ..Intent here is to have pluggable script, which will do its work based on present plugins.
-* ..Next needed piece is xml (Components.xml), where basic needed information will be stored.
-* ....Information about IsProduct, whould be moved from ConfiguredModules.xml into Components.xml.
-* ......Or even better is should be ported all...
-* ..GetInstalledProducts will return also version as it make perfect sense right now.
 
 * Ok, ultimate decision is that each plugin will represent each component and all these will be dynamicaly loaded
 * ..-> based on these will be installed products determined plus their versions
@@ -29,7 +25,6 @@
 * ......ExtractZipFile.ps1 (DESCRIPTION: function for extracting zip files)
 * ..Orion Platform data (DESCRIPTION: container for all Orion Platform data files)
 * ....Components.xml (DESCRIPTION: all important information about Orion Platform components is stored here)
-* ....ConfiguredModules.xml (DESCRIPTION: configuration IDs for Orion Platform products)
 * ..Orion Platform functions (DESCRIPTION: container for all Orion Platform specific functions)
 * ....GetInstalledProducts.ps1 (DESCRIPTION: function for getting all installed products)
 * ..Tools (DESCRIPTION: container for all tools composed from functions, data files and so on)
